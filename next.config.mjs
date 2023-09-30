d9cd1ee5465fae29e8e6db5176ccf4db30f49d62
Justin Henry <jdhenry08@gmail.com>
2023-09-30 01:13:25 +0000

    What

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,13 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-
 await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
   pageExtensions: ["tsx", "ts", "jsx", "js"],
   reactStrictMode: true,
+  output: "export",
 };
 
 export default config;
